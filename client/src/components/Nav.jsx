fcbaabb84ecdf59d7b78e2b8f57c6cc8e3486451
Fred <fsolleder3@gmail.com>
2023-11-25 07:14:17 -0800

    fixing eslint errors

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -1,18 +1,19 @@
-import React from 'react';
+import { Link, useLocation } from 'react-router-dom';
 
-function Nav({ currentSection, setCurrentSection }) {
-  const navTitles = ['About Me', 'Portfolio', 'Contact', 'Resume'];
+function Nav() {
+  const location = useLocation();
+  const navTitles = [
+    { title: 'About Me', path: '/' },
+    { title: 'Contact', path: '/contact' },
+    { title: 'Resume', path: '/resume' }
+  ];
 
   return (
     <nav>
-      {navTitles.map((title) => (
-        <button
-          key={title}
-          className={currentSection === title.replace(' ', '') ? 'highlight' : ''}
-          onClick={() => setCurrentSection(title.replace(' ', ''))}
-        >
+      {navTitles.map(({ title, path }) => (
+        <Link key={title} to={path} className={location.pathname === path ? 'highlight' : ''}>
           {title}
-        </button>
+        </Link>
       ))}
     </nav>
   );
