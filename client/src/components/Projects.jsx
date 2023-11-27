6e105ef2691e0b07c29e814cc7f3ca5f07b3dc93
Fred <fsolleder3@gmail.com>
2023-11-27 07:48:17 -0800

    modified projects section

--- client/src/components/Projects.jsx
+++ client/src/components/Projects.jsx
@@ -1,10 +1,49 @@
+import starImage from '../assets/star1.png'
+import weatherApp from '../assets/weatherapp.jpg'
+import movieCave from '../assets/mc1.png'
+import jate from '../assets/jate.png'
+import tfire from '../assets/tfire.jpg'
+import sq from '../assets/sq.jpg'
+
+
+
 function Projects() {
   const projects = [
     {
-      title: "Thing One",
-      image: "",
-      liveLink: "",
-      githubLink: ""
+      title: "STAR",
+      image: starImage,
+      liveLink: "https://social-media-app123-a735270541c9.herokuapp.com/",
+      githubLink: "https://github.com/fspaghetti3/social-media-app"
+    },
+    {
+      title: "Weather App",
+      image: weatherApp, 
+      liveLink: "https://fspaghetti3.github.io/weather-app/",
+      githubLink: "https://github.com/fspaghetti3/weather-app"
+    },
+    {
+      title: "MovieCave",
+      image: movieCave,
+      liveLink: "https://fspaghetti3.github.io/movie-project/",
+      githubLink: "https://github.com/fspaghetti3/movie-project"
+    },
+    {
+      title: "J.A.T.E",
+      image: jate,
+      liveLink: "https://jate-1-005c2bacda69.herokuapp.com/",
+      githubLink: "https://github.com/fspaghetti3/jate"
+    },
+    {
+      title: "TekFire",
+      image: tfire,
+      liveLink: "https://tekfire-d5ba20163777.herokuapp.com/",
+      githubLink: "https://github.com/fspaghetti3/tech-blog2"
+    },
+    {
+      title: "Spooky Quiz",
+      image: sq,
+      liveLink: "https://fspaghetti3.github.io/quiz-game/",
+      githubLink: "https://github.com/fspaghetti3/quiz-game"
     },
   ];
 
